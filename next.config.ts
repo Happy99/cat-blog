da3d3c1904c1b86c2d7870faa2db6afb87c07c2e
Petr Stastny <ps.happy99@gmail.com>
2025-03-31 22:46:01 +0200

    Update Next.js configuration to include redirects, remove unused index page, and refactor imports for consistency

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-};
+  redirects: async () => [
+    {
+      source: '/',
+      destination: '/articles',
+      permanent: true,
+    },
+  ],
+}
 
-export default nextConfig;
+export default nextConfig
