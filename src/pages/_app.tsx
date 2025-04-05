78cfdaa303d44eb271f774c8d9484f669456d2df
Petr Stastny <ps.happy99@gmail.com>
2025-04-05 12:48:54 +0200

    fix: Capital letter for component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from '@/components/layout/Layout'
+import Layout from '@/components/Layout/Layout'
 import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
